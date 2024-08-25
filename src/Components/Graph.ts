10a03316301f5339a3ede2ef350b0d64e7ccfbfc
Branden Evangelista <bevangelista@ufl.edu>
2024-08-25 16:44:08 -0700

    feat(graph): create graph class
    Constructor to create a root node for the graph
    
    Contains a Map of nodes to keep track of all edges
    
    Functions to add neighbors and edges

--- src/Components/Graph.ts
+++ src/Components/Graph.ts
@@ -1,5 +1,33 @@
+import { Node } from "./Node"
 class Graph<T> {
+    root: Node<T>;
+    nodes: Map<T, Node<T>> = new Map<T, Node<T>>;
 
+    constructor(data: T) {
+        this.root = new Node<T>(data);
+    }
+    
+    addNode(data: T): Node<T> {
+        let node = this.nodes.get(data);
+        if (node) {
+            return node;
+        }
+
+        node = new Node<T>(data);
+        this.nodes.set(data, node);
+        return node;
+    }
+
+    addEdge(source: T, destination: T): void {
+        const sourceNode = this.nodes.get(source);
+        const destinationNode = this.nodes.get(source);
+
+        if (sourceNode) {
+            sourceNode.addNeighbor(destinationNode);
+        } else {
+            console.error("ERROR: Source node is not defined");
+        }
+    }
 }
 
 export { Graph }
