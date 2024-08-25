e653384585b8a495193b3b394cfa524519991760
Branden Evangelista <bevangelista@ufl.edu>
2024-08-25 16:25:58 -0700

    feat(node): create node class
    Constructor to initialize value of node and neighbors
    
    Created function to add adjacent neighbors to each node

--- src/Components/Node.ts
+++ src/Components/Node.ts
@@ -1,5 +1,15 @@
 class Node<T> {
+    data: T;
+    adjNodes: Node<T>[];
 
+    constructor(data: T) {
+        this.data = data;
+        this.adjNodes = new Array<Node<T>>;
+    }
+
+    addNeighbor(node: Node<T>) : void {
+        this.adjNodes.push(node);
+    }
 }
 
 export { Node }
