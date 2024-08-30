7b0592299ecaf6f08654ae8592b6781f8157b2c2
Branden Evangelista <44141229+bkevangelista@users.noreply.github.com>
2024-08-29 21:10:14 -0700

    Feat/player (#2)
    * feat(player): create player class
    - Player class keeps track of number of pieces, number of points, and all edges that belong to player
    - create General constants file
    - Rework edge class to take a List instead of a Set
    - Rework other classes to fit with change from Edge class
    
    * style(player): add newline between imports and class declaration
    
    * refactor(player): change order of addEdgeForPlayer parameters
    - Change order of parameters to match that of addEdge in the Graph class
    
    * fix(player): change directory from import
    - replace parent path with .. in import declaration
    
    * test(index): Verify player class functionality by declaring object in index.ts

---
+++ src/Constants/GeneralConstants.ts
@@ -0,0 +1,8 @@
+export const NUM_PIECES = 45;
+export const PIECES_TO_POINTS = {
+    1 : 1,
+    2 : 2,
+    3 : 4,
+    5 : 10,
+    6 : 15
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Graph } from "./Components/Graph";
 import { City } from "./Constants/City";
 import { Color } from "./Constants/Color";
+import { Player } from "./Components/Player";
 
 const graph = new Graph(City.Phoenix);
 graph.addNode(City.Seattle);
@@ -8,8 +9,13 @@ graph.addNode(City.Vancouver);
 graph.addNode(City.Portland);
 graph.addEdge(City.Seattle, City.Phoenix, 2, Color.Blue);
 graph.addEdge(City.Seattle, City.Vancouver, 1, Color.Wild);
-graph.addEdge(City.Seattle, City.Portland, 1, Color.Wild);
+graph.addEdge(City.Seattle, City.Portland, 6, Color.Wild);
 
 console.log(graph.edges);
 
 graph.dfs();
+
+const playerOne: Player = new Player();
+playerOne.addEdgeForPlayer(City.Seattle, City.Phoenix, 2, Color.Blue);
+playerOne.addEdgeForPlayer(City.Seattle, City.Portland, 6, Color.Wild);
+console.log(playerOne);
